a6fdb4787212fa109288fda5cb13cbde8c92f8e9
vaibhavvats85 <vaibhavvats85@gmail.com>
2020-12-13 16:51:08 +0530

    test added

--- src/tests/components/programs/programs.test.js
+++ src/tests/components/programs/programs.test.js
@@ -13,16 +13,59 @@ jest.mock('react-redux', () => ({
 jest.mock('react-router-dom', () => ({
     useLocation: () => jest.fn(),
     useHistory: () => jest.fn()
-}))
+}));
+
 beforeEach(() => {
     wrapper = shallow(<Programs />);
-
 });
+
 test('programs component should render correctly', () => {
     expect(wrapper).toMatchSnapshot();
 });
 
 test('should render cards if data is present', () => {
-    const card = wrapper.find('Cards');
-    expect(card).toBeTruthy();
+    const cards = wrapper.find('Cards');
+    expect(cards).toBeTruthy();
+});
+
+test('should render loader if data is present', () => {
+    jest.mock('react-redux', () => ({
+        useDispatch: jest.fn(),
+        useSelector: () => ({
+            data: [],
+            hasError: false,
+            loading: true
+        })
+    }));
+    const wrapper = shallow(<Programs />);
+    const loader = wrapper.find('Loader');
+    expect(loader).toBeTruthy();
+});
+
+test('should render error if data is present', () => {
+    jest.mock('react-redux', () => ({
+        useDispatch: jest.fn(),
+        useSelector: () => ({
+            data: [],
+            hasError: true,
+            loading: false
+        })
+    }));
+    const wrapper = shallow(<Programs />);
+    const error = wrapper.find('Error');
+    expect(error).toBeTruthy();
+});
+
+test('should render noprograms if data is present', () => {
+    jest.mock('react-redux', () => ({
+        useDispatch: jest.fn(),
+        useSelector: () => ({
+            data: [],
+            hasError: false,
+            loading: false
+        })
+    }));
+    const wrapper = shallow(<Programs />);
+    const noprograms = wrapper.find('Noprograms');
+    expect(noprograms).toBeTruthy();
 });
