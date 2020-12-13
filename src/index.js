5edfc191cbf47abbb434d9e00749d4ebab9dc49f
vaibhavvats85 <vaibhavvats85@gmail.com>
2020-12-13 14:36:26 +0530

    build issues resolved

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.hydrate(
   <Provider store={store}>
     <Router>
